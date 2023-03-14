bae39ae8414010e44d6e4a87dd9fab2213be2a16
神代綺凛 <i@loli.best>
2023-03-14 11:38:59 +0800

    fix: romaji

--- src/utils/romaji.ts
+++ src/utils/romaji.ts
@@ -2,22 +2,24 @@
 import Kuroshiro from 'kuroshiro';
 // @ts-expect-error
 import KuromojiAnalyzer from 'kuroshiro-analyzer-kuromoji';
-import { reduce } from 'lodash';
 
 const kuroshiro = new Kuroshiro();
 const init = kuroshiro.init(new KuromojiAnalyzer());
 
 const extraTable = {
-  遊: 'yu',
-  濯: 'taku',
+  遊龍: 'yuryu',
+  濯塵: 'takujin',
+  承曦: 'shoasahi',
 };
 
 export const getRomaji = async (text: string): Promise<string> => {
   if (/^[\w-]*$/.test(text)) return '';
   await init;
-  return reduce(
-    extraTable,
-    (result, romaji, kanji) => result.replace(kanji, romaji),
-    await kuroshiro.convert(text, { to: 'romaji', romajiSystem: 'passport' }),
-  );
+  for (const [kanji, romaji] of Object.entries(extraTable)) {
+    if (text.startsWith(kanji)) {
+      text = text.replace(kanji, romaji);
+      break;
+    }
+  }
+  return kuroshiro.convert(text, { to: 'romaji', romajiSystem: 'passport' });
 };
